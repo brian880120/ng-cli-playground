533d783e68f2b2cdd6fae332f5a37f9f978f7795
brian880120 <bobriandai@gmail.com>
2017-05-16 22:20:06 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CustomerComponent } from './customer/customer.component';
+import { OrdersComponent } from './orders/orders.component';
+import { AdminComponent } from './admin/admin.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+    { path: '', pathMatch: 'full', redirectTo: 'customers' },
+    { path: 'customers', component: CustomerComponent },
+    { path: 'orders', component: OrdersComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { LoginModule } from './login/login.module';
+import { AdminModule } from './admin/admin.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { OrdersComponent } from './orders/orders.component';
+import { CustomerService } from './customer/customer.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    LoginModule,
+    AdminModule
   ],
-  providers: [],
+  providers: [CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
